6d504ecd4c56dc71081411d154b82adb93a409f5
ohwoo-kwon <kwonow1@gmail.com>
2022-06-05 19:18:24 +0900

    2.6 Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,19 @@
-const Profile = () => <span>Profile</span>;
+import { authService } from "fbase";
+import { signOut } from "firebase/auth";
+import { useHistory } from "react-router-dom";
+
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    signOut(authService);
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
