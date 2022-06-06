521886c48cc37a436a8a547cb67690a3033b165a
ohwoo-kwon <kwonow1@gmail.com>
2022-06-06 13:16:29 +0900

    #3.1 Tweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,35 @@
-const Home = () => <span>Home</span>;
+import { dbService } from "fbase";
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
+
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await addDoc(collection(dbService, "tweets"), {
+      tweet,
+      createdAt: Date.now(),
+    });
+    setTweet("");
+  };
+  const onChange = (e) => {
+    setTweet(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={tweet}
+          onChange={onChange}
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
